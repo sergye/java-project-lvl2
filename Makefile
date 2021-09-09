1a14b91434ce5a0c1656366833e964e5755492ec
sergye <sergye@gmail.com>
2021-09-09 09:11:09 +0300

    Step 3

---
+++ Makefile
@@ -0,0 +1,16 @@
+install:
+	./gradlew clean install
+
+run-dist:
+	./build/install/app/bin/app
+
+check-updates:
+	./gradlew dependencyUpdates
+
+lint:
+	./gradlew checkstyleMain
+
+build:
+	./gradlew clean build
+
+.PHONY: build
--- src/main/java/hexlet/code/App.java
+++ src/main/java/hexlet/code/App.java
@@ -1,7 +1,19 @@
 package hexlet.code;
 
-public class App {
+import java.util.concurrent.callable;
+import picocli.CommandLine;
+
+@CommandLine.Command(name = "gendiff", mixinStandardHelpOptions = true, version = "1.0",
+        description = "Compares two configuration files and shows a difference.")
+public class App implements Callable<Integer> {
+
+    @Override
+    public Integer call() {
+        return 0;
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        int exitCode = new CommandLine(new App()).execute(args);
+        System.exit(exitCode);
     }
 }
